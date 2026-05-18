27e6cd9fd3062a18b6021ab18d7e5036fa9bcf30
Brian Griffin <brian.griffin@gmail.com>
2026-05-18 16:20:32 -0600

    added variations as features.

--- extra.rs
+++ extra.rs
@@ -7,7 +7,22 @@ impl From<Icon> for char {
 /// Material icons font (`MaterialIcons-Regular.ttf`), licensed under the Apache 2.0 license
 /// While attribution is not strictly required, it is appreciated.
 /// See https://github.com/google/material-design-icons for more information
-pub const FONT: &[u8] = include_bytes!("../assets/MaterialIcons-Regular.ttf");
+
+#[cfg(all(feature = "outlined", not(feature = "rounded"), not(feature = "sharp")))]
+pub const FONT: &[u8] = include_bytes!("../assets/MaterialSymbolsOutlined.woff2");
+
+#[cfg(all(feature = "rounded", not(feature = "outlined"), not(feature = "sharp")))]
+pub const FONT: &[u8] = include_bytes!("../assets/MaterialSymbolsRounded.woff2");
+
+// Default
+#[cfg(any(
+feature = "sharp",
+all(not(feature = "outlined"), not(feature = "rounded"), not(feature = "sharp")),
+          all(feature = "outlined", feature = "rounded"),
+          all(feature = "outlined", feature = "sharp"),
+          all(feature = "rounded", feature = "sharp"),
+))]
+pub const FONT: &[u8] = include_bytes!("../assets/MaterialSymbolsSharp.woff2");
 
 use std::fmt;
 impl fmt::Display for Icon {
